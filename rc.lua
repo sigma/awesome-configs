0685e67a41b1624fa3daf9d29d90f8905baf38d9
Adrian C. (anrxc) <anrxc@sysphere.org>
2009-12-07 16:05:59 +0100

    rc.lua: removed focus fix for obsolete pre_manage patch

--- rc.lua
+++ rc.lua
@@ -562,9 +562,6 @@ client.add_signal("manage", function (c, startup)
             awful.placement.no_overlap(c)
             awful.placement.no_offscreen(c)
         end
-
-        -- Fix for pre_manage rules patch
-        client.focus = c
     end
 end)
 -- }}}
