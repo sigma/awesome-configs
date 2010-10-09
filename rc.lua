729c9e7063bf688e7c4b7e9267ae17b61a68a258
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-10-09 19:13:43 +0200

    rc.lua: upgraded to awesome v3.4.8

--- rc.lua
+++ rc.lua
@@ -1,6 +1,6 @@
 -- {{{ License
 --
--- Awesome configuration, using awesome 3.4.7 on Arch GNU/Linux
+-- Awesome configuration, using awesome 3.4.8 on Arch GNU/Linux
 --   * Adrian C. <anrxc@sysphere.org>
 
 -- Screenshot: http://sysphere.org/gallery/snapshots
