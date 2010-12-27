461892a8d637fc10d7f1ddddc4bc27d443663e22
Adrian C. (anrxc) <anrxc@sysphere.org>
2010-12-28 00:22:38 +0100

    rc.lua: new tag setup for easier property expansion

--- rc.lua
+++ rc.lua
@@ -51,10 +51,11 @@ tags = {
 }}
 
 for s = 1, screen.count() do
-    tags[s] = awful.tag(tags.names, s, tags.layout)
-    awful.tag.setproperty(tags[s][5], "mwfact", 0.13)
-    awful.tag.setproperty(tags[s][6], "hide",   true)
-    awful.tag.setproperty(tags[s][7], "hide",   true)
+  tags[s] = awful.tag(tags.names, s, tags.layout)
+  for i, t in ipairs(tags[s]) do
+      awful.tag.setproperty(t, "mwfact", i==5 and 0.13  or  0.5)
+      awful.tag.setproperty(t, "hide",  (i==6 or  i==7) and true)
+  end
 end
 -- }}}
 
